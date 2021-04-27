cd9285b1e69634ad6274c0511b1b167d403f6d56
Kirill Klenov <horneds@gmail.com>
2021-04-27 15:46:45 +0300

    fix: wip

--- asgi_tools/forms.py
+++ asgi_tools/forms.py
@@ -111,7 +111,7 @@ class MultipartReader(FormReader):
 
             else:
                 self.partdata = f = SpooledTemporaryFile(self.file_memory_limit)  # type: ignore
-                f._file.name = options['filename']
+                f._file.name = options['filename']  # type: ignore
 
             f.content_type = self.headers[b'content-type'].decode(self.charset)  # type: ignore
 
